4b90412f490d6d5e31b25646f9644f7a586a6170
Ebby Wahman <ebbywahman@gmail.com>
2013-12-15 16:52:24 -0500

    Changed Dialog Handling

--- Assets/Scripts/DialogTrigger.cs
+++ Assets/Scripts/DialogTrigger.cs
@@ -4,27 +4,43 @@ using System.Collections;
 public class DialogTrigger : MonoBehaviour
 {
 	public int size;
+	
 	public string[] textDisplay;
+	public int currentTextIndex;
+	
 	public bool isTriggered;
 	public float waitTime;
 	private bool dialogOver;
 	private GameObject dialogBox;
+	
 	private TextMesh[] text;
+	private int mainTextIndex;
 	
 	void Start ()
 	{	
 		dialogOver = false;
+		currentTextIndex = 0;
 		dialogBox = GetComponentInChildren<SpriteRenderer> ().gameObject;
 		dialogBox.renderer.enabled = false;
 		text = GetComponentsInChildren<TextMesh> ();
-		foreach (TextMesh tex in text) {
-			tex.renderer.enabled = false;
+		for (int i = 0; i < text.Length; i++) {
+			text [i].renderer.enabled = false;
+			if (text [i].name.Contains ("Line 1")) {
+				mainTextIndex = i;
+				text [i].text = textDisplay [currentTextIndex];
+			}
 		}
 	}
 	
-	void Update ()
-	{
-	
+	public void next ()
+	{	
+		if (currentTextIndex < textDisplay.Length - 1) {
+			currentTextIndex++;
+			text [mainTextIndex].text = textDisplay [currentTextIndex];
+		} else {
+			dialogOver = true;
+			closeDialog ();
+		}
 	}
 	
 	public void openDialog ()
