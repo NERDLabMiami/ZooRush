4b68d697541b87d4123914c71f47450e35a8131c
Ebby Wahman <ebbywahman@gmail.com>
2013-12-22 17:21:11 -0500

    added essential scene manager properties to level editor window

--- Assets/Editor/LevelEditor.cs
+++ Assets/Editor/LevelEditor.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 
 public class LevelEditor : EditorWindow
 {
-	bool levelEditorEnable = false;
+	private SceneManager sceneManager;
+	private bool levelEditorEnable = false;
+	private bool scoreVarFoldout = false;
 	
 	[MenuItem("Zoo Rush Tools/Level Editor %l")]
 	private static void showEditor ()
@@ -22,7 +24,9 @@ public class LevelEditor : EditorWindow
 	
 	void Update ()
 	{
-	
+		if (sceneManager == null) {
+			sceneManager = GameObject.FindObjectOfType<SceneManager> ();
+		}
 	}
 
 	void OnGUI ()
@@ -33,6 +37,28 @@ public class LevelEditor : EditorWindow
 			levelEditorEnable = EditorGUILayout.Toggle (levelEditorEnable);
 		}
 		EditorGUILayout.EndHorizontal ();
+		if (levelEditorEnable) {
+			if (sceneManager != null) {
+				EditorGUILayout.Space ();
+				EditorGUILayout.BeginVertical ();
+				{
+					EditorGUILayout.LabelField ("Scene Manager Properties for " + Application.loadedLevelName);
+				
+					sceneManager.NextSceneName = EditorGUILayout.TextField ("Next Scene Name:", sceneManager.NextSceneName);
+					sceneManager.isEndless = EditorGUILayout.ToggleLeft ("Endless Mode", sceneManager.isEndless);
+					sceneManager.tutEnabled = EditorGUILayout.ToggleLeft ("Tutorial Mode", sceneManager.tutEnabled);
+					scoreVarFoldout = EditorGUILayout.Foldout (scoreVarFoldout, "Scoring Variables");
+					
+					if (scoreVarFoldout) {	
+						sceneManager.targetTimeVar = EditorGUILayout.FloatField ("Target Time (3 stars):", sceneManager.targetTimeVar);	
+						sceneManager.multiplier1 = EditorGUILayout.FloatField ("Time multiplier (2 stars):", sceneManager.multiplier1);
+						sceneManager.multiplier2 = EditorGUILayout.FloatField ("Time multiplier (1 star):", sceneManager.multiplier2);
+					}
+				}
+				EditorGUILayout.EndVertical ();
+				EditorGUILayout.Space ();
+			}
+		}
 		EditorGUILayout.BeginHorizontal ();
 		{
 			if (GUILayout.Button ("Reset Player Prefs")) {
