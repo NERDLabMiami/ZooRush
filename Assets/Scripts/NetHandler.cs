244c65e2e753138a0120b8d6a0c9e0c9b9441fb9
Ebby Wahman <ebbywahman@gmail.com>
2013-12-28 02:02:56 -0500

    some net work
    still thinking of a better system

--- Assets/Scripts/NetHandler.cs
+++ Assets/Scripts/NetHandler.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetHandler : MonoBehaviour {
+public class NetHandler : MonoBehaviour
+{
+	
+	public Sprite[] sprites;
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (rigidbody2D.velocity.y < 0f) {
+			Debug.Log ("DOWN!!!");
+			GetComponent<SpriteRenderer> ().sprite = sprites [1];
+		}
 	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+//		Debug.Log (other.gameObject.name);
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		Debug.Log (collision.gameObject.name);
+//		if (collision.gameObject.name.Contains ("Animal") != null) {
+//			Debug.Log ("AMINAL!");
+//		}
+	}
+	
 }
