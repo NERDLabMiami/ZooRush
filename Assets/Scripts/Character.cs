2d3be58a846d9bb7c047e2592aaf7b3b1151cd43
Ebby Wahman <ebbywahman@gmail.com>
2014-02-04 01:44:15 -0500

    updated animal graphics

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+
+//	public Sprite[] sprites;
+//	private int currentCharacter;
+//	private string[] characters = {"David", "Lisa", "Christina","Zane"};
+//
+//	void Awake ()
+//	{
+//		if (PlayerPrefs.HasKey ("Character Selected")) {
+//			string character = PlayerPrefs.GetString ("Character Selected");
+//			for (int i = 0; i < characters.Length; i++) {
+//				if (characters [i].Equals (character)) {
+//					currentCharacter = i;
+//				}
+//			}
+//		} else {
+//			currentCharacter = 0;
+//		}
+//		gameObject.GetComponent<SpriteRenderer> ().sprite = sprites [currentCharacter];
+//	}
+//	
+//	void Update ()
+//	{
+//	
+//	}
+}
