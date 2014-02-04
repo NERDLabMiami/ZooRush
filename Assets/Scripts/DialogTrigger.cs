de29a811464e8bada9b4f70e67cd37e30ad02981
Ebby Wahman <ebbywahman@gmail.com>
2014-02-04 17:20:53 -0500

    fixed dialog boxes
    found 3 dialog handlers in the camera for some reason

--- Assets/Scripts/DialogTrigger.cs
+++ Assets/Scripts/DialogTrigger.cs
@@ -4,24 +4,29 @@ using System.Collections;
 public class DialogTrigger : MonoBehaviour
 {
 	public bool tutOnly;
+	public bool opened;
 	
 	public string[] textDisplay;
 	private string[] formatted;
 	public int currentTextIndex;
 	
-	public bool isTriggered;
-	public float waitTime;
-	private bool dialogOver;
+	public bool dialogOver;
 	private GameObject dialogBox;
-	
+
 	private TextMesh[] text;
 	private int mainTextIndex;
-	
+
+
+	private SceneManager sceneManager;
+	private DialogHandler dialogHandler;
 	void Start ()
 	{	
 		dialogOver = false;
+		opened = false;
 		currentTextIndex = -1;
 		dialogBox = GetComponentInChildren<SpriteRenderer> ().gameObject;
+		sceneManager = GameObject.FindObjectOfType<SceneManager> ();
+		dialogHandler = GameObject.FindObjectOfType<DialogHandler> ();
 		dialogBox.renderer.enabled = false;
 		text = GetComponentsInChildren<TextMesh> ();
 		for (int i = 0; i < text.Length; i++) {
@@ -37,7 +42,6 @@ public class DialogTrigger : MonoBehaviour
 	{	
 		if (currentTextIndex < textDisplay.Length - 1) {
 			currentTextIndex++;
-			Debug.Log ("LINE 1");
 			if (textDisplay [currentTextIndex] [0].Equals ('+')) {
 				text [mainTextIndex].text = textDisplay [currentTextIndex].Substring (1);
 			} else {
@@ -45,13 +49,11 @@ public class DialogTrigger : MonoBehaviour
 			}
 			if (currentTextIndex + 1 < textDisplay.Length) {
 				if (textDisplay [currentTextIndex + 1] [0].Equals ('+')) {
-					Debug.Log ("LINE 2");
 					text [mainTextIndex].text += "\n" + textDisplay [currentTextIndex + 1].Substring (1);
 					currentTextIndex++;
 					if (currentTextIndex + 1 < textDisplay.Length) {
 						
 						if (textDisplay [currentTextIndex + 1] [0].Equals ('+')) {
-							Debug.Log ("LINE 3");
 							text [mainTextIndex].text += "\n" + textDisplay [currentTextIndex + 1].Substring (1);
 							currentTextIndex++;
 						}
@@ -76,18 +78,25 @@ public class DialogTrigger : MonoBehaviour
 	
 	public void openDialog ()
 	{
-		dialogBox.renderer.enabled = true;
-		foreach (TextMesh tex in text) {
-			tex.renderer.enabled = true;
+		if (!opened) {
+			dialogBox.renderer.enabled = true;
+			foreach (TextMesh tex in text) {
+				tex.renderer.enabled = true;
+			}
+			opened = true;
 		}
+
 	}
 	
-	public void closeDialog ()
+	private void closeDialog ()
 	{
 		dialogBox.renderer.enabled = false;
 		foreach (TextMesh tex in text) {
 			tex.renderer.enabled = false;
 		}
+		sceneManager.isPlaying = true;
+		dialogHandler.displaying = false;
+		dialogHandler.found = false;
 	}
 	
 	public bool isDialogFinished ()
