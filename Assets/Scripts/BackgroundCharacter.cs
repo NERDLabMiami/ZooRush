ac07320e3475974cddf722db598e296aa16a8285
Ebby Wahman <ebbywahman@gmail.com>
2014-02-08 01:42:27 -0500

    added updated downtown sprites

--- Assets/Scripts/BackgroundCharacter.cs
+++ Assets/Scripts/BackgroundCharacter.cs
@@ -7,7 +7,7 @@ public class BackgroundCharacter : MonoBehaviour
 	public bool anyCharacter;
 
 	private int chosenCharacter;
-
+	private bool left;
 	void Awake ()
 	{
 		chosenCharacter = Random.Range (0, 4);
@@ -22,16 +22,29 @@ public class BackgroundCharacter : MonoBehaviour
 			while (chosenCharacter  == PlayerPrefs.GetInt ("Character Selected")) {
 				chosenCharacter = Random.Range (0, 4);
 			}
+			GetComponent<Animator> ().SetInteger ("Character", chosenCharacter);
+			GetComponent<Animator> ().SetTrigger ("Change");
 		}
-
-		rigidbody2D.velocity = new Vector2 (8f, 0);
+		if (transform.parent.localScale.x > 0) {
+			rigidbody2D.velocity = new Vector2 (8f, 0);
+			left = false;
+		} else {
+			rigidbody2D.velocity = new Vector2 (-8f, 0);
+			left = true;
+		}
 
 	}
 	
 	void Update ()
 	{
-		if (transform.position.x > Camera.main.transform.position.x + 25f) {
-			Destroy (transform.parent.gameObject);
+		if (left) {
+			if (transform.position.x < Camera.main.transform.position.x - 25f) {
+				Destroy (transform.parent.gameObject);
+			}
+		} else {
+			if (transform.position.x > Camera.main.transform.position.x + 25f) {
+				Destroy (transform.parent.gameObject);
+			}
 		}
 	}
 }
