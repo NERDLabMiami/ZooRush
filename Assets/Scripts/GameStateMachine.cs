14691875c52765a65348bb3c19adf8169ad67eeb
ebbyw <ebbywahman@gmail.com>
2014-02-14 22:24:13 -0500

    started game state machine work

---
+++ Assets/Scripts/GameStateMachine.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Controls the Game's current state
+ */ 
+public class GameStateMachine : MonoBehaviour
+{
+
+	enum GameState
+	{
+		Intro,
+		Play,
+		Paused,
+		Transition,
+		EndLevel}
+	;
+
+	static int currentState;
+
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public static void RequestPlay(){
+
+	}
+
+	public static void RequestPause(){
+
+	}
+
+	public static void RequestAnimationTransition(){
+
+	}
+
+	public static void Request
+}
