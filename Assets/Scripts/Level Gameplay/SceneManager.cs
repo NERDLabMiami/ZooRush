1b9cfaf12d19d2df71de87aedd2f1afefa837be1
ebbyw <ebbywahman@gmail.com>
2014-05-31 17:14:29 -0400

    purge/refactor beginnings

---
+++ Assets/Scripts/Level Gameplay/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SceneManager : MonoBehaviour
+{
+		public string NextSceneName; 		//Filename of the next scene 
+
+		
+		public bool timedOut;
+		public float targetTimeVar;
+		public float multiplier1;
+		public float multiplier2;
+	
+		public Animal animalController;
+		public GameObject characterObject;
+		public GameObject animalObject;
+		public CameraFollow cameraFollow;
+	
+		public bool isPlaying;
+		public bool pauseAudio;
+		protected bool tutEnabled;
+		public bool fainted;
+
+		public void updatePillCount (int pillCount)
+		{
+				string theCount = "x" + pillCount;
+				TextMesh pillCountText = GameObject.Find ("Pill Count").GetComponentInChildren<TextMesh> ();
+				pillCountText.text = theCount;
+		}
+
+}
