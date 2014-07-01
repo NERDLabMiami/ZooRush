ebc86bc4b9f488af0ce0b953d7bbd3d4d7747d9a
Ebby Wahman <ebbywahman@gmail.com>
2014-06-30 23:32:12 -0400

    some more help menu work

---
+++ Assets/HelpMenuWaterSet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpMenuWaterSet : HelpMenuSet
+{
+
+	public PowerUp[] waterBottles;
+
+	private Vector3 originalPosition;
+	
+	void OnEnable ()
+	{
+		originalPosition = transform.position;
+	}
+	
+	public override void activate ()
+	{
+		transform.localPosition = Vector3.zero;
+	}
+	
+	public override void dismiss ()
+	{
+		transform.position = originalPosition;
+	}
+	
+	public override void reset ()
+	{
+		
+	}
+}
