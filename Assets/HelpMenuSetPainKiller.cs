dd15b36ac8994355e0909e39166e0e51b1eb38fd
ebbyw <ebbywahman@gmail.com>
2014-07-01 16:32:11 -0400

    more help menu work

---
+++ Assets/HelpMenuSetPainKiller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpMenuSetPainKiller : HelpMenuSet
+{
+		public PainKiller painkiller;
+
+		public override void activate ()
+		{
+				throw new System.NotImplementedException ();
+		}
+
+		public override void dismiss ()
+		{
+				throw new System.NotImplementedException ();
+		}
+
+		public override void reset ()
+		{
+				throw new System.NotImplementedException ();
+		}
+}
