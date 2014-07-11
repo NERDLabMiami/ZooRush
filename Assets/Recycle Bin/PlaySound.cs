815832336925830248e97081cb0104151d34256e
Ebby Wahman <ebbywahman@gmail.com>
2014-07-11 01:00:07 -0400

    organizing

---
+++ Assets/Recycle Bin/PlaySound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : ObjectModel
+{
+	public AudioClip clip;
+	private bool touched;
+
+
+	new void Start ()
+	{
+		base.Start ();
+
+		touched = false;
+		audioController = GameObject.FindObjectOfType<AudioController> ();
+	}
+
+	protected override void resetOtherValues ()
+	{
+		touched = false;
+	}
+	public override void collisionDetected ()
+	{
+
+	}
+	public override void interactWithCharacter (GameObject character)
+	{
+		if (!touched) {
+			audioController.objectInteraction (clip);
+			touched = true;
+		}
+	}
+
+}
